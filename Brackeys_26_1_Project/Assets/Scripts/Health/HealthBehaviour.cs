e3cae1eff132dbddd546b5ae1e93465e0996552c
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2026-02-15 11:52:43 -0300

    scripts added.

---
+++ Brackeys_26_1_Project/Assets/Scripts/Health/HealthBehaviour.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthBehaviour : MonoBehaviour
+{
+    [SerializeField] protected bool _isInvincible = false;
+    [SerializeField] protected int _maxHealth = 100;
+    [SerializeField] protected UnityEvent<HealthBehaviour> OnHurt_Event = null;
+    [SerializeField] protected UnityEvent<HealthBehaviour> OnDead_Event = null;
+    //[SerializeField] protected int _defaultMaxHealth = 100;
+
+    [Header("// READONLY")]
+    [SerializeField] protected int _currentHealth = 0;
+
+    public event UnityAction OnHurt = null;
+    public event UnityAction OnDead = null;
+    public event UnityAction OnRestored = null;
+
+    protected virtual void Awake()
+    {
+        RestoreAllHealth();
+    }
+
+    public void TakeDamage(int _value)
+    {
+        if (!IsAlive()) return;
+
+        _currentHealth -= _value;
+
+        if (_isInvincible)
+            RestoreAllHealth();
+
+        if (_currentHealth <= 0)
+        {
+            OnDead_();
+        }
+        else
+        {
+            OnDamageTaken_();
+        }
+    }
+
+    public void RestoreAllHealth()
+    {
+        RestoreHealth(999);
+    }
+
+    public void RestoreHealth(int _percentage)
+    {
+        var _value = _maxHealth * (_percentage / 100f);
+        _currentHealth += Mathf.RoundToInt(_value);
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        OnRestored?.Invoke();
+    }
+
+    protected virtual void OnDead_()
+    {
+        _currentHealth = 0;
+        OnDead?.Invoke();
+        OnDead_Event?.Invoke(this);
+    }
+
+    protected virtual void OnDamageTaken_()
+    {
+        OnHurt?.Invoke();
+        OnHurt_Event?.Invoke(this);
+    }
+
+    public bool IsAlive()
+    {
+        return _currentHealth > 0;
+    }
+
+    public virtual float GetNormalizedValue()
+    {
+        return _currentHealth / (_maxHealth * 1f);
+    }
+
+    // UPGRADES
+    public void IncreaseMaxHealth(int _percentage)
+    {
+        //var _buff = _defaultMaxHealth * (_percentage / 100f);
+        //_maxHealth += (int)_buff;
+        _maxHealth += _percentage;
+    }
+
+    //public void DecreaseMaxHealth(int _percentage)
+    //{
+    //    var _buff = _defaultMaxHealth * (_percentage / 100f);
+    //    _maxHealth -= (int)_buff;
+    //    _maxHealth = Mathf.Clamp(_maxHealth, 1, 9999);
+    //}
+}
---
+++ Brackeys_26_1_Project/Assets/Scripts/Health/PlayerHealth.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerHealth : HealthBehaviour
+{
+    [Header("// PLAYER")]
+    [SerializeField] float _timeRate = 1f;
+    [SerializeField] int _decreaseRate = 1;
+    [SerializeField] UnityEvent _onPlayerDead = null;
+
+    private float _timer = 0f;
+
+    public static event UnityAction OnPlayerHurt = null;
+    public static event UnityAction OnPlayerDead = null;
+
+    protected override void Awake()
+    {
+        RestoreAllHealth();
+    }
+
+    private void LateUpdate()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer > _timeRate)
+        {
+            _timer = 0;
+            TakeDamage(_decreaseRate);
+        }
+    }
+
+    protected override void OnDamageTaken_()
+    {
+        base.OnDamageTaken_();
+        OnPlayerHurt?.Invoke();
+    }
+
+    protected override void OnDead_()
+    {
+        base.OnDead_();
+        gameObject.SetActive(false);
+        OnPlayerDead?.Invoke();
+        _onPlayerDead?.Invoke();
+    }
+
+    internal void SetInvincibility(bool _value)
+    {
+        _isInvincible = _value;
+    }
+
+    //public void UpdateMaxHealth(int _health)
+    //{
+    //    _maxHealth += _health;
+
+    //    if(_currentHealth > _maxHealth)
+    //        _currentHealth = _maxHealth;
+    //}
+}
